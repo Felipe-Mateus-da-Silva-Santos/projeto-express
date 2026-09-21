47942d20d696aa4286790f527738321a0b858033
Daniele-Dionisio <danieledionisio34@gmail.com>
2026-09-20 22:46:56 -0400

    edição:  quarta rota get

--- API.js
+++ API.js
@@ -3,9 +3,9 @@ const API = express();
 const PORTA = 3000;
 
 const tarefas = [
-    {id: 1 , titulo:'Escovar os dentes', status:'Pendente'},
-    {id: 2 , titulo:'Tomar banho', status:'Concluida'},
-    {id: 3 , titulo:'Ir pra escola', status:'Pendente'}
+    {id: 1 , titulo:'Escovar os dentes', Concluida: false },
+    {id: 2 , titulo:'Tomar banho', Concluida: true},
+    {id: 3 , titulo:'Ir pra escola', Concluida: false}
 ];
 
 API.get('/', (req, res) => {
@@ -19,12 +19,20 @@ API.get('/tarefas', (req, res) => {
 API.get('/tarefas:id', (req, res) => {
     const id = req.params.id;
     const tarefa = tarefas.find(
-        u => u.id === parseInt(id)
+        t => t.id === parseInt(id)
     );
     if (!tarefa){
        return res.statusCode(404).json({error:'Tarefa não encontrada'});
     }
-    res.status(200).json(tarefas)
+    res.statusCode(200).json(tarefas)
 });
 
+API.get('/tarefas', (req, res) => {
+    const { Concluida } = req.query;
+    const tarefasFiltradas = Concluida=true
+        ? tarefas.filter(t => t.Concluida === Concluida)
+        : tarefas;
+
+    res.json(tarefasFiltradas);
+});
 
