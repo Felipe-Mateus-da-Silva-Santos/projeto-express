7c3accc5a6a68ebe283d296a7f0ebfcc3ab4c0fe
Daniele-Dionisio <danieledionisio34@gmail.com>
2026-09-20 22:07:34 -0400

    edição: segunda rota get criada

--- API.js
+++ API.js
@@ -2,6 +2,16 @@ const express = require('express');
 const API = express();
 const PORTA = 3000;
 
+const tarefas = [
+    {id: 1 , titulo:'Escovar os dentes', status:'Pendente'},
+    {id: 2 , titulo:'Tomar banho', status:'Concluida'},
+    {id: 3 , titulo:'Ir pra escola', status:'Pendente'}
+];
+
 API.get('/', (req, res) => {
     res.send('API de tarefas no ar!');
+});
+
+API.get('/tarefas', (req, res) => {
+    res.send(tarefas);
 });
